48f2eba10e9d4f446cb90f7a10775da593156d78
John Salaveria <jj.salaveria@gmail.com>
2016-10-16 14:49:56 -0400

    modal display logic

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,16 +1,55 @@
-console.log('\'Allo \'Allo!');
+(function($){
 
-function autoheight(a) {
-    console.log('here');
-    if (!$(a).prop('scrollTop')) {
-        do {
-            var b = $(a).prop('scrollHeight');
-            var h = $(a).height();
-            $(a).height(h - 5);
+    $.fn.extend({
+        animateCss: function (animationName) {
+            var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+            this.addClass('animated ' + animationName).one(animationEnd, function() {
+                $(this).removeClass('animated ' + animationName);
+            });
         }
-        while (b && (b != $(a).prop('scrollHeight')));
-    };
-    $(a).height($(a).prop('scrollHeight') + 20);
-}
+    });
 
-autoheight($('#ta'));
+    var Modal = function ($elt){
+        this.active = false;
+        this.$modal = $elt;
+        this.response = {};
+        this.$submit = this.$modal.find('input[type="button"]').first();
+
+        this.$submit.click((e)=>{
+            e.preventDefault();
+            this.$modal.addClass('u-hidden');
+        });
+
+        function delayAnimate($items, animation, interval = 0) {
+            var delay = 0;
+
+            $items.addClass('u-invisible');
+
+            console.log('delaying now');
+
+            $.each($items, function(i, item) {
+                setTimeout(function() {
+                    $(item).removeClass('u-invisible');
+                    $(item).animateCss(animation);
+                }, delay);
+                delay+= interval;
+            })
+        }
+
+        return {
+            showModal : ()=>{
+                console.log('clicked');
+                this.$modal.removeClass('u-hidden');
+                delayAnimate(this.$modal.children(), 'fadeInUp');
+                delayAnimate(this.$modal.find('form > *'), 'fadeInUp', 250);
+            }
+        };
+    };
+
+    $(document).ready(function(){
+        var newEntry = new Modal($('.new-entry'));
+
+        $('.newpost').click(newEntry.showModal);
+    });
+
+})(jQuery);
