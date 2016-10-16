8e52f226439a3a2be9e1c4ac39e929e77c22b4fa
Jessica Hanta <jesshanta@gmail.com>
2016-10-16 11:38:36 -0400

    added textarea

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,16 @@
 console.log('\'Allo \'Allo!');
+
+function autoheight(a) {
+    console.log('here');
+    if (!$(a).prop('scrollTop')) {
+        do {
+            var b = $(a).prop('scrollHeight');
+            var h = $(a).height();
+            $(a).height(h - 5);
+        }
+        while (b && (b != $(a).prop('scrollHeight')));
+    };
+    $(a).height($(a).prop('scrollHeight') + 20);
+}
+
+autoheight($('#ta'));
