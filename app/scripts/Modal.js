dd3f1addb230c0e9afd69347366dad103744a061
John Salaveria <jj.salaveria@gmail.com>
2016-11-16 19:02:54 -0500

    added browserify, fixed animation bug

---
+++ app/scripts/Modal.js
@@ -0,0 +1,56 @@
+export default function($, elt, openerClass){
+    this.active = false;
+    this.$modal = $(elt);
+    this.response = {};
+    this.$opener = $(openerClass);
+    this.$clone = this.$modal.clone(true);
+    this.$submit = this.$modal.find('input[type="button"]').first();
+
+    this.registerOpener = ()=> {
+        this.$opener.click(this.showModal);
+    }
+
+    this.$submit.click((e)=>{
+        let $modal = this.$modal;
+        e.preventDefault();
+
+        //validate form here
+
+        $modal.find('.form-container').addClass('u-hidden');
+        $modal.find('.success').removeClass('u-hidden');
+        delayAnimate($modal.find('.success').children(), 'fadeInUp');
+
+        setTimeout(function(){
+
+            $modal.animateCss('fadeOut', function() {
+                $modal.addClass('u-hidden');
+                $modal.find('.form-container').removeClass('u-hidden');
+                $modal.find('.success').addClass('u-hidden');
+            });
+        }, 1050);
+        
+    });
+
+    function delayAnimate($items, animation, interval = 0) {
+        var delay = 0;
+
+        $items.addClass('u-invisible');
+
+        $.each($items, function(i, item) {
+            setTimeout(function() {
+                $(item).removeClass('u-invisible');
+                $(item).animateCss(animation);
+            }, delay);
+            delay+= interval;
+        })
+    }
+
+    this.showModal = ()=> {
+        this.$modal.removeClass('u-hidden');
+        delayAnimate(this.$modal.children(), 'fadeInUp');
+        delayAnimate(this.$modal.find('form > *'), 'fadeInUp', 250);
+    }
+
+    //initialize modal
+    this.registerOpener();
+};
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,55 +1,23 @@
+import Modal from './Modal';
+
 (function($){
 
     $.fn.extend({
-        animateCss: function (animationName) {
+        animateCss: function (animationName, callback) {
             var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
             this.addClass('animated ' + animationName).one(animationEnd, function() {
                 $(this).removeClass('animated ' + animationName);
+
+                if (callback){
+                    callback();
+                    $(this).off(animationEnd);
+                }
             });
         }
     });
 
-    var Modal = function ($elt){
-        this.active = false;
-        this.$modal = $elt;
-        this.response = {};
-        this.$submit = this.$modal.find('input[type="button"]').first();
-
-        this.$submit.click((e)=>{
-            e.preventDefault();
-            this.$modal.addClass('u-hidden');
-        });
-
-        function delayAnimate($items, animation, interval = 0) {
-            var delay = 0;
-
-            $items.addClass('u-invisible');
-
-            console.log('delaying now');
-
-            $.each($items, function(i, item) {
-                setTimeout(function() {
-                    $(item).removeClass('u-invisible');
-                    $(item).animateCss(animation);
-                }, delay);
-                delay+= interval;
-            })
-        }
-
-        return {
-            showModal : ()=>{
-                console.log('clicked');
-                this.$modal.removeClass('u-hidden');
-                delayAnimate(this.$modal.children(), 'fadeInUp');
-                delayAnimate(this.$modal.find('form > *'), 'fadeInUp', 250);
-            }
-        };
-    };
-
     $(document).ready(function(){
-        var newEntry = new Modal($('.new-entry'));
-
-        $('.newpost').click(newEntry.showModal);
+        var newEntry = new Modal( jQuery, '.new-entry', '.newpost');
     });
 
 })(jQuery);
