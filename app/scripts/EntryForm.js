4b1a02f999a7713a2ec9fb1af1d9c4b5a45f96ec
Jessica Hanta <jesshanta@gmail.com>
2016-11-21 21:14:14 -0500

    made fixes

--- app/scripts/EntryForm.js
+++ app/scripts/EntryForm.js
@@ -74,8 +74,8 @@ export default function(){
     
     $('.inactive').on('focus', function activeArea() { 
         var selectedTextArea = document.activeElement;
-        // console.log(selectedTextArea);
-        // console.log(selectedTextArea.id);
+        console.log(selectedTextArea);
+        console.log(selectedTextArea.id);
         currId=selectedTextArea.id;
         $('.number-inactive#n'+currId).toggleClass('number-active');
         console.log('.number-inactive#n'+currId);
@@ -84,7 +84,7 @@ export default function(){
 
 
     $('.inactive').on('focusout', function inactiveArea() {
-        // console.log(currId);
+        console.log(currId);
         $('.number-inactive#n'+currId).toggleClass('number-active');
         updateCharCount('');
     });
@@ -92,57 +92,7 @@ export default function(){
     $('.inactive').on('keyup', function countChar() {
         updateCharCount(maxChar-this.value.length);
     });
-
-    $('#submit').click(function submitForm(){
-        //check input boxes
-        // console.log('here submit');
-        var t1 = document.getElementById('ta1');
-        var t2 = document.getElementById('ta2');
-        var t3 = document.getElementById('ta3');
-
-        // console.log(t1);
-        // console.log(t2);
-        // console.log(t3);
-
-        var regexp=/^[a-zA-Z]+(([\'\,\.\- ][a-zA-Z ])?[a-zA-Z]*)*$/;
-
-        if(t1.value==null||t2.value==null||t3.value==null){
-            //output error message
-            document.getElementById('err').innerHTML = 'Please fill in 3 entries';
-        }
-        else if((maxChar-t1.value.length)<0){
-            document.getElementById('err').innerHTML = 'First entry exceeds max length';
-        }
-        else if((maxChar-t2.value.length)<0){
-            document.getElementById('err').innerHTML = 'Second entry exceeds max length';
-        }
-        else if((maxChar-t3.value.length)<0){
-            document.getElementById('err').innerHTML = 'Third entry exceeds max length';
-        }
-        else if(!validator.isAlphanumeric(t1.value)){
-            console.log('invalid chars1');
-            document.getElementById('err').innerHTML = 'First entry contains invalid characters';
-        }
-        else if(!validator.isAlphanumeric(t2.value)){
-            console.log('invalid chars2');
-            document.getElementById('err').innerHTML = 'Second entry contains invalid characters';
-        }
-        else if(!validator.isAlphanumeric(t3.value)){
-            console.log('invalid chars3');
-            document.getElementById('err').innerHTML = 'Third entry contains invalid characters';
-        }
-        else{
-            var allentries = [];//{t1.value, t2.value, t3.value};
-            allentries.push(t1.value);
-            allentries.push(t2.value);
-            allentries.push(t3.value);
-            document.getElementById('err').innerHTML = '';
-            document.getElementById('ta1').innerHTML ='';
-            document.getElementById('ta2').innerHTML ='';
-            document.getElementById('ta3').innerHTML ='';
-            return allentries;
-        }
-    });
+    //comment
 }
 
 
--- app/scripts/Modal.js
+++ app/scripts/Modal.js
@@ -5,6 +5,7 @@ import {delayAnimate} from './util/animation'
 let api = ApiUtil();
 
 export default function(elt, openerClass){
+    var maxChar=170;
     this.active = false;
     this.$modal = $(elt);
     this.response = {};
@@ -20,9 +21,43 @@ export default function(elt, openerClass){
         let $modal = this.$modal;
         e.preventDefault();
 
+        var t1 = $('#ta1').val();
+        var t2 = $('#ta2').val();
+        var t3 = $('#ta3').val();
+
+
+        if(t1.length==0 || t2.length==0||t3.length==0){
+            $('#err').html('Please write your 3 entries');
+            return;
+        }
+
+        //check lengths
+        if((maxChar-t1.length)<0){
+            $('#err').html('First entry exceeds max length');
+            return;
+        }
+        else if((maxChar-t2.length)<0){
+            $('#err').html('Second entry exceeds max length');
+            return;
+        }
+        else if((maxChar-t3.length)<0){
+            $('#err').html('Third entry exceeds max length');
+            return;
+        }
+
+
+        //code injection avoidance convert to html entities
+        t1 = removeInject(t1);
+        t2 = removeInject(t1);
+        t3 = removeInject(t1);
+
+        var allentries = [];//{t1.value, t2.value, t3.value};
+        allentries.push(t1);
+        allentries.push(t2);
+        allentries.push(t3);
+
         //validate form here
         api.postUser('Joanne');
-        api.postEntry(null);
 
         $modal.find('.form-container').addClass('u-hidden');
         $modal.find('.success').removeClass('u-hidden');
@@ -39,7 +74,9 @@ export default function(elt, openerClass){
         
     });
 
-    
+    function removeInject( html ) {
+        return $( $.parseHTML(html) ).text();
+    }
 
     this.showModal = ()=> {
         this.$modal.removeClass('u-hidden');
